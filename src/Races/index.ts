5520cdeb517ba55ff3b1f64395c881973b947306
cafe51 <cafecafe51@hotmail.com>
2022-11-30 19:23:26 -0400

    requisito 2 resolvido

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,15 @@
 import Race from './Race';
+import Orc from './Orc';
+import Halfling from './Halfling';
+import Dwarf from './Dwarf';
+import Elf from './Elf';
 
 export default Race;
+
+export {
+  Race,
+  Orc,
+  Halfling,
+  Dwarf,
+  Elf,
+};
