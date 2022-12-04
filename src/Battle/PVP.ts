34bb689f8d6aedf75aaf68e5e1816ed916277a6a
cafe51 <cafecafe51@hotmail.com>
2022-12-03 20:47:00 -0400

    requisito 10 resolvido

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,27 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+// import Character from '../Character';
+
+export default class PVP extends Battle {
+  private _fighter1: Fighter;
+  private _fighter2: Fighter;
+
+  constructor(fighter1: Fighter, fighter2: Fighter) {
+    super(fighter1);
+    this._fighter1 = fighter1;
+    this._fighter2 = fighter2;
+  }
+
+  fight(): number {
+    this._fighter1.attack(this._fighter2);
+    if (this._fighter2.lifePoints === -1) {
+      return 1;
+    } this._fighter2.attack(this._fighter1);
+
+    if (this._fighter1.lifePoints === -1) {
+      return -1;
+    } this.fight();
+    // if (this._fighter1.lifePoints > 0) this.fight();
+    return 0;
+  }
+}
